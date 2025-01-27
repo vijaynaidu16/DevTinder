a69db34500d9c4bc3a7587b1005f7dfe5664970f
vijay <digitalxvijay@gmail.com>
2025-01-27 20:43:39 +0530

    Schema Types

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.post("/signup", async (req, res) => {
     res.send(`User added successfully`);
   } catch (err) {
     console.error("Error adding user:", err);
-    res.status(500).send("Error adding user to the database");
+    res.status(500).send("Error adding user to the database" + err);
   }
 });
 
@@ -23,23 +23,61 @@ app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
   console.log(userEmail);
   try {
-    const users = await userModel.find({ emailId: userEmail });
-    if(users.length === 0) res.send("user not found")
-    res.send(users).status(200);
+    const users = await userModel.findOne({ emailId: userEmail });
+    if (!users) {
+      res.status(404).send("user not found");
+    }
+    // res.send(users).status(200);
   } catch (error) {
     res.status(404).send("something went wrong");
   }
 });
 
-app.get("/feed", async (req, res) => {
+app.get("/getid", async (req, res) => {
+  const userId = req.query.userId; // Use query parameter for GET request
+  try {
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res.send("not found");
+    }
+    res.send("found the id");
+  } catch (error) {
+    res.status(404).send("Error");
+  }
+});
 
+app.get("/feed", async (req, res) => {
   try {
     const users = await userModel.find({});
     res.send(users).status(200);
   } catch (error) {
     res.status(404).send("something went wrong");
   }
-})
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await userModel.findByIdAndDelete(userId);
+    res.status(200).send("user deleted");
+  } catch (error) {
+    res.status(404);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await userModel.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+      returnDocuement: "after",
+    });
+    res.status(200).send("Patched the data");
+  } catch (error) {
+    res.status(404).send("something went wrong" + error.message);
+  }
+});
 
 connectDB()
   .then(() => {
--- src/model/userSchema.js
+++ src/model/userSchema.js
@@ -1,31 +1,61 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+    },
 
-  lastName: {
-    type: String,
-  },
+    lastName: {
+      type: String,
+    },
 
-  emailId: {
-    type: String,
-  },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
 
-  password: {
-    type: String,
-  },
+    password: {
+      type: String,
+      required: true,
+    },
 
-  age: {
-    type: String,
-    required: false, // Make it optional
+    age: {
+      type: Number,
+      min: 18,
+    },
+
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is invalid");
+        }
+      },
+    },
+
+    photoUrl: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkkE1LSJXHRCOSWyn1wFq8RhGSO8_geSROkA&s",
+    },
+
+    about: {
+      type: String,
+      default: "This is about the user",
+    },
+
+    skills: {
+      type: [String],
+    },
   },
-  gender: {
-    type: String,
-    required: false, // Make it optional
-  },
-});
+  { timestamps: true },
+);
 
 const userModel = mongoose.model("User", userSchema);
-module.exports = {userModel};
+module.exports = { userModel };
